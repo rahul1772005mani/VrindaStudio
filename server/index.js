d403cf955e4568fed8c815c44167bd4d135a903b
Rahul Naik <rahul1772005mani@gmail.com>
2026-07-08 23:16:22 +0530

    Updated project

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5005;
 
 // Initialize Razorpay client
 const razorpay = new Razorpay({
